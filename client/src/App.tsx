5eead0d51c9103e3251d2807e261123375fb03f0
anson <ansonbenny166@gmail.com>
2023-12-29 22:42:21 +0530

    initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.scss'
+
+function App() {
+  return (
+    <section>
+      <h1 className='text-2xl font-bold text-blue-400'>CRM</h1>
+    </section>
+  )
+}
+
+export default App
