9e511a795db1610f882a04b84651a2be45f995fe
anson <ansonbenny166@gmail.com>
2024-01-10 18:48:52 +0530

    api config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,26 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-    port: 4173,
-    strictPort: true,
-    watch: {
-      usePolling: true
+export default ({ mode }) => {
+  process.env = { ...process?.env, ...loadEnv(mode, process?.cwd?.(), '') }
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      host: true,
+      port: 4173,
+      strictPort: true,
+      watch: {
+        usePolling: true
+      },
+      proxy: {
+        '/api': {
+          target: process?.env?.BACK_END,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      }
     }
-  }
-})
+  })
+}
