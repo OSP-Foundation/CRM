7fae29a04d6b4a3f2e05b50969c135f8f5d5e795
Althaf <althafrahim101@gmail.com>
2024-01-12 21:49:23 +0530

    auth card center

--- client/src/components/auth/layout/index.tsx
+++ client/src/components/auth/layout/index.tsx
@@ -9,11 +9,11 @@ interface props {
 
 const Authlayout = ({ children, title, subtitle }: props) => {
   return (
-    <div className="flex flex-col md:flex-row  w-full p-[1.5rem] md:p-[3rem] m-auto items-center justify-center bg-primary-bg">
+    <div className="flex flex-col md:flex-row min-h-[100vh] w-full p-[1.5rem] md:p-[3rem] m-auto items-center justify-center bg-primary-bg">
       <div className="max-w-xl w-full hidden md:block">
         <Background width={"100%"} height={"100%"} />
       </div>
-      <div className="max-w-sm w-full bg-white shadow-primary-box-shadow p-7 rounded-md flex flex-col items-center">
+      <div className="my-auto max-w-sm w-full bg-white shadow-primary-box-shadow p-7 rounded-md flex flex-col items-center">
         <div className="py-4 flex flex-col w-full gap-1  text-center">
           <h2 className="text-lg capitalize text-pure-black font-bold">
             {title}
